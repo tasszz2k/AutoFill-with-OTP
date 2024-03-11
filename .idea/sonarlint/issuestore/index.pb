536b59dacb05bf20b366fb8059bcedb7694dfe6c
tasszz2k <tasszz2k@gmail.com>
2024-03-11 14:31:07 +0700

    update(autofill): active extension only on url matches urlPattens

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+E
+js/content/content.js,5/d/5d9b73c2fa74c5c57895991ab9dc75aa8e762e92
+K
+js/background/background.js,a/2/a2120ee8bfe9e6b9114f133ee065e151555c1493
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
--- js/content/content.js
+++ js/content/content.js
@@ -1,4 +1,4 @@
-console.log("AutoFill_with_OTP_Extension content script is running.");
+// console.log("AutoFill_with_OTP_Extension content script is running.");
 
 const username_fields = ['username', 'login_username', 'user_login', 'user', 'login-form-username', 'os_username', 'Username'];
 const password_fields = ['password', 'login_password', 'user_pass', 'login-form-password', 'os_password', 'Password', 'PIN + Google OTP'];
@@ -39,8 +39,17 @@ function autoFillFields() {
 }
 
 window.addEventListener('load', function () {
-    console.log("Window loaded. Now attempting to autofill...");
-    setTimeout(autoFillFields, 1000);
+    // get current URL
+    let currentURL = window.location.href;
+    console.log("Current URL:", currentURL);
+    shouldFillPassword(currentURL, function (shouldFill) {
+        if (shouldFill) {
+            console.log("Window loaded. Now attempting to autofill...");
+            setTimeout(autoFillFields, 1000);
+        } else {
+            // console.log("This page does not require autofill.");
+        }
+    });
 });
 
 
@@ -56,3 +65,24 @@ function getSelectorString(selectors) {
     return selectors.map(selector => `input[name="${selector}"], input[id="${selector}"], input[placeholder="${selector}"]`).join(', ');
 }
 
+// read urlPatterns from storage
+// compare the current url with the urlPatterns (check contains string)
+function shouldFillPassword(url, callback) {
+    chrome.storage.sync.get(['urlPatterns'], function (data) {
+        let urlPatterns = data.urlPatterns;
+        // check if urlPatterns is empty, return true
+        if (!urlPatterns || urlPatterns.length === 0) {
+            callback(true);
+            return;
+        }
+
+        for (let i = 0; i < urlPatterns.length; i++) {
+            if (url.includes(urlPatterns[i])) {
+                console.log("URL matched with pattern", urlPatterns[i]);
+                callback(true);
+                return;
+            }
+        }
+        callback(false);
+    });
+}
